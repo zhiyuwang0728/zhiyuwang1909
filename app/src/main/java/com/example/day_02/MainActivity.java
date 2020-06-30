0d81d61698c3d8f19184e58b124ac262670d9a61
unknown <2415109806@qq.com>
2020-06-30 10:12:28 +0800

    first

---
+++ app/src/main/java/com/example/day_02/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.day_02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView tv_title;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv_title = findViewById(R.id.tv_title);
+        Button btn_click = findViewById(R.id.btn_click);
+        btn_click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv_title.setText("欢迎你");
+            }
+        });
+
+    }
+}
